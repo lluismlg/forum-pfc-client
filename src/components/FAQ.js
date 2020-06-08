a70465425e98444881542cb382d106a3c548d898
lluismlg <lluismlg97@gmail.com>
2020-06-08 16:44:34 +0200

    fixed erratas faq

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -13,28 +13,33 @@ class FAQ extends Component {
 			"faqs": [
 				{
 					"categoria": "General",
+					"pregunta": "What does the 'Cs' in 'forumCs' stand for?",
+					"respuesta": "It stands for 'Castellón' which is where I'm from"
+				},
+				{
+					"categoria": "General",
 					"pregunta": "What's the main topic in this forum?",
-					"respuesta": "This forum was designed as a tool to help citizens solve common problems, such as bureaucracy, economic or politic ones "
+					"respuesta": "This forum was designed as a tool to help citizens solve common problems, such as bureaucracy, economic or political ones"
 				},
 				{
 					"categoria": "Privacy",
 					"pregunta": "Who can see my posts or comments?",
-					"respuesta": "Anyone can see them so it's more lickely it will reach someone who can help you, or that you can reach someone you may be able to help "
+					"respuesta": "Anyone can see them so it's more likely it will reach someone who can help you, or that you can reach someone you may be able to help"
 				},
 				{
 					"categoria": "Account",
 					"pregunta": "Will we be able to log in with our google accounts in the future?",
-					"respuesta": "That's a posibility we have thought about but we lack the time to work on it at the moment"
+					"respuesta": "That's a possibility we have thought about but we lack the time to work on it at the moment"
 				},
 				{
 					"categoria": "General",
-					"pregunta": "Im having problems working my way around the web, can I get any help?",
-					"respuesta": "I'd recommend checking out the sitemap of our website, you can acces it from the footer at any page.If you are still having problems please send us an email from the contact page once we implement it"
+					"pregunta": "I'm having problems working my way around the web, can I get any help?",
+					"respuesta": "I'd recommend checking out the sitemap of our website, you can access it from the footer at any page. If you are still having problems please send us an email from the contact page once we implement it"
 				},
 				{
 					"categoria": "General",
 					"pregunta": "How do you make money?",
-					"respuesta": "We are planning on adding a feature which will allow as to put ads in the feed"
+					"respuesta": "We are planning on adding a feature which will allow us to put ads in the feed"
 				}
 			]
 		}
